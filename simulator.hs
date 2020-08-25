c9d3451358eb5d6f455f489be2f5f592f830daeb
41hz <dm41is@protonmail.ch>
2020-08-25 19:29:23 -0400

    cleaning up code and fixing a small issue in the exponentiation

--- simulator.hs
+++ simulator.hs
@@ -1,11 +1,11 @@
 data Complex = Complex {
-    real :: Double,
+    real      :: Double,
     imaginary :: Double
 } deriving Show
 
 data Polar = Polar {
     magnitude :: Double,
-    angle :: Double
+    angle     :: Double
 } deriving Show
 
 addComplex :: Complex -> Complex -> Complex
@@ -15,24 +15,24 @@ subtractComplex :: Complex -> Complex -> Complex
 subtractComplex (Complex r1 i1) (Complex r2 i2) = Complex (r1 - r2) (i1 - i2)
 
 multiplyComplex :: Complex -> Complex -> Complex
-multiplyComplex (Complex r1 i1) (Complex r2 i2) = Complex ((r1 * r2) - (i1 * i2)) ((r1 * i2) + (i1 * r2))
+multiplyComplex (Complex r1 i1) (Complex r2 i2) = Complex (r1*r2 - i1*i2) (r1*i2 + i1*r2)
 
 divideComplex :: Complex -> Complex -> Complex
-divideComplex (Complex r1 i1) (Complex 0 0) = error "Division by zero"
+divideComplex (Complex _ _) (Complex 0 0) = error "Division by zero"
 divideComplex (Complex r1 i1) (Complex r2 i2) =
-    let denominator = r2^2 + i2^2
-    in Complex (((r1 * r2) + (i1 * i2))/denominator) (((i1 * r2) - (r1 * i2))/denominator)
+    let denominator = r2*r2 + i2*i2
+    in Complex ((r1*r2 + i1*i2)/denominator) ((i1*r2 - r1*i2)/denominator)
 
 conjugateComplex :: Complex -> Complex
 conjugateComplex (Complex r i) = Complex r (-i)
 
 modulusComplex :: Complex -> Double
-modulusComplex (Complex r i) = sqrt(r^2 + i^2)
+modulusComplex (Complex r i) = sqrt (r*r + i*i)
 
 cartesianToPolar :: Complex -> Polar
 cartesianToPolar (Complex r i)
     | r > 0 = Polar (modulusComplex (Complex r i)) (atan i/r)
-    | r < 0 = Polar (modulusComplex (Complex r i)) ((atan i/r) + pi)
+    | r < 0 = Polar (modulusComplex (Complex r i)) (atan i/r + pi)
     | otherwise = Polar (abs i) (signum i * 0.5 * pi)
 
 polarToCartesian :: Polar -> Complex
@@ -42,12 +42,13 @@ multiplyPolar :: Polar -> Polar -> Polar
 multiplyPolar (Polar m1 a1) (Polar m2 a2) = Polar (m1 * m2) (a1 + a2)
 
 dividePolar :: Polar -> Polar -> Polar
-dividePolar (Polar m1 a1) (Polar 0 a2) = error "Division by zero"
+dividePolar (Polar _ _) (Polar 0 _)     = error "Division by zero"
 dividePolar (Polar m1 a1) (Polar m2 a2) = Polar (m1 / m2) (a1 - a2)
 
 powerPolar :: Polar -> Int -> Polar
 powerPolar (Polar m a) e
-| e >= 1 || e == 0 = Polar (m^e) (e*a)
-| otherwise = error "Only positive integers allowed in the exponent"
+    | e >= 1 || e == 0 = Polar (m^e) (a * fromIntegral e)
+    | otherwise = error "Only positive integers allowed in the exponent"
 
-main = print (cartesianToPolar (Complex (-2) (-2)))
+main :: IO ()
+main = print (modulusComplex (Complex (-1) 1))
